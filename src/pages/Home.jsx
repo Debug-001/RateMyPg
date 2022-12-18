a1482fb507a3f2bbe6084096c23401438edddbc1
Om Bhatt <ombhatt1158@gmail.com>
2022-12-18 17:18:09 +0530

    defined rotes and bootstrap inititated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
+import {BsHouse} from 'react-icons/bs'
 
-const hOME = () => {
+
+
+
+const Home = () => {
   return (
-    <div>
-        <h1>RatemyDorn</h1>
+    <div className="containe-fluid navbar-style py-4" >
+      <div className="row">
+        <div className="col-md-12 col-12 ">
+          <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+              <div className="icon">
+                <BsHouse color="black" size={26}/>
+                </div>
+              <a class="navbar-text" href="/">RateMyPG.com</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="/">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/">Link</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/">About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/">Explore</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+
+        </div>
+      </div>
     </div>
   )
 }
 
-export default hOME
+export default Home
