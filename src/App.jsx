616f8cb4f728651ca57a7e2fec59cff4c817f552
Om Bhatt <ombhatt1158@gmail.com>
2022-12-18 15:17:15 +0530

    File structuring commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
-const App = () => {
+
+function App () {
   return (
     <>
-    
-    </>
+   <Router>
+   <Routes>
+       <Route path='/' element={<Home/>} />
+   </Routes>
+   </Router>
+   </>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const hOME = () => {
+  return (
+    <div>
+        <h1>RatemyDorn</h1>
+    </div>
+  )
+}
+
+export default hOME
